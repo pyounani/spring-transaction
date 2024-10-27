4a44aac56817d19a8b271cbd9b3e69e67ec22cad
pyounani <yndbsk9372@gmail.com>
2024-10-27 10:02:26 +0900

    [Setting] 프로젝트 초기 설정

---
+++ src/test/java/pyounani/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,13 @@
+package pyounani.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
